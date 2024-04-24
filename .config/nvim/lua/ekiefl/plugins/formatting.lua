d2b62ac18c997bbc1acaec633292884f797b158f
Evan Kiefl <kiefl.evan@gmail.com>
2024-04-24 10:45:29 -0700

    Wed Apr 24 10:30:42 PDT 2024

--- .config/nvim/lua/ekiefl/plugins/formatting.lua
+++ .config/nvim/lua/ekiefl/plugins/formatting.lua
@@ -4,44 +4,43 @@ local TIMEOUT = 3000
 -- Don't autoformat when saving for these filetypes
 -- (call <leader>af if you want to format)
 local NO_FORMAT_ON_SAVE = {
-  "python",
-  "lua",
+    "python",
 }
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-    conform.setup({
-      -- :ConformInfo to see log file
-      log_level = vim.log.levels.ERROR,
-      formatters_by_ft = {
-        python = { "ruff" },
-        lua = { "my_stylua" },
-      },
-      format_on_save = function(bufnr)
-        -- Disable autoformat on certain filetypes
-        if vim.tbl_contains(NO_FORMAT_ON_SAVE, vim.bo[bufnr].filetype) then
-          return
-        end
-        -- ...additional logic...
-        return { timeout_ms = TIMEOUT, lsp_fallback = true, async = false }
-      end,
-      formatters = {
-        nick_nack = {
-          command = "patty_wack",
-        },
-      },
-    })
+        conform.setup({
+            -- :ConformInfo to see log file
+            log_level = vim.log.levels.ERROR,
+            formatters_by_ft = {
+                python = { "ruff_fix", "ruff_format" },
+                lua = { "my_stylua" },
+            },
+            format_on_save = function(bufnr)
+                -- Disable autoformat on certain filetypes
+                if vim.tbl_contains(NO_FORMAT_ON_SAVE, vim.bo[bufnr].filetype) then
+                    return
+                end
+                -- ...additional logic...
+                return { timeout_ms = TIMEOUT, lsp_fallback = true, async = false }
+            end,
+            formatters = {
+                nick_nack = {
+                    command = "patty_wack",
+                },
+            },
+        })
 
-    vim.keymap.set({ "n", "v" }, "<leader>af", function()
-      conform.format({
-        lsp_fallback = false,
-        async = false,
-        timeout_ms = TIMEOUT,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+        vim.keymap.set({ "n", "v" }, "<leader>af", function()
+            conform.format({
+                lsp_fallback = false,
+                async = false,
+                timeout_ms = TIMEOUT,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
