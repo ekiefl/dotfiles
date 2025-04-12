92f05324cf427e283105c7c9f69cf4e6afda615c
Evan Kiefl <kiefl.evan@gmail.com>
2025-04-12 14:35:41 -0700

    Fri Apr 11 15:07:28 PDT 2025

--- .config/nvim/lua/ekiefl/plugins/leap.lua
+++ .config/nvim/lua/ekiefl/plugins/leap.lua
@@ -4,5 +4,20 @@ return {
 	config = function()
 		local leap = require("leap")
 		leap.set_default_mappings()
+
+		leap.opts.case_sensitive = true
+
+		-- Define equivalence classes for brackets and quotes, in addition to
+		-- the default whitespace group:
+		require("leap").opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
+
+		-- Use the traversal keys to repeat the previous motion without
+		-- explicitly invoking Leap:
+		require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+
+		-- Define a preview filter (skip the middle of alphanumeric words):
+		require("leap").opts.preview_filter = function(ch0, ch1, ch2)
+			return not (ch1:match("%s") or ch0:match("%w") and ch1:match("%w") and ch2:match("%w"))
+		end
 	end,
 }
